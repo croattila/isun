589a5c856b8db2df69139ff6e5141632424b54e1
Attila Horvath <croattila@yahoo.com>
2016-04-19 18:56:18 +0100

    updateWeather()

--- app/src/main/java/com/example/hp/isun/ForecastFragment.java
+++ app/src/main/java/com/example/hp/isun/ForecastFragment.java
@@ -1,9 +1,11 @@
 package com.example.hp.isun;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -29,8 +31,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Encapsulates fetching the forecast and displaying it as a {@link ListView} layout.
@@ -61,12 +61,20 @@ public class ForecastFragment extends Fragment {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
-            FetchWeatherTask weatherTask = new FetchWeatherTask();
-            //weatherTask.execute("94043");
-            weatherTask.execute("London");
+            updateWeather();
+
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void updateWeather() {
+        FetchWeatherTask wheaterTask = new FetchWeatherTask();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String location = prefs.getString(getString(R.string.pref_location_key)
+        ,getString(R.string.pref_location_default));
+        wheaterTask.execute(location);
+
     }
 
     @Override
@@ -74,16 +82,7 @@ public class ForecastFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         // Create some dummy data for the ListView.  Here's a sample weekly forecast
-        String[] data = {
-                "Mon 6/23 - Sunny - 31/17",
-                "Tue 6/24 - Foggy - 21/8",
-                "Wed 6/25 - Cloudy - 22/17",
-                "Thurs 6/26 - Rainy - 18/11",
-                "Fri 6/27 - Foggy - 21/10",
-                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                "Sun 6/29 - Sunny - 20/7"
-        };
-        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
 
         // Now that we have some dummy forecast data, create an ArrayAdapter.
         // The ArrayAdapter will take data from a source (like our dummy forecast) and
@@ -93,7 +92,7 @@ public class ForecastFragment extends Fragment {
                         getActivity(), // The current context (this activity)
                         R.layout.list_item_forecast, // The name of the layout ID.
                         R.id.list_item_forecast_view, // The ID of the textview to populate.
-                        weekForecast);
+                        new ArrayList<String>());
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
@@ -110,11 +109,16 @@ public class ForecastFragment extends Fragment {
                 startActivity(intent);
 
 
-
             }
         });
 
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateWeather();
     }
 
 
@@ -155,7 +159,7 @@ public class ForecastFragment extends Fragment {
                 throws JSONException {
 
             // These are the names of the JSON objects that need to be extracted.
-             final String OWM_LIST = "list";
+            final String OWM_LIST = "list";
             final String OWM_WEATHER = "weather";
             final String OWM_TEMPERATURE = "temp";
             final String OWM_MAX = "max";
@@ -326,9 +330,9 @@ public class ForecastFragment extends Fragment {
 
         @Override
         protected void onPostExecute(String[] result) {
-            if (result != null){
+            if (result != null) {
                 mForecastAdapter.clear();
-                for (String dayForecastStr : result){
+                for (String dayForecastStr : result) {
                     mForecastAdapter.add(dayForecastStr);
                 }
             }
--- app/src/main/java/com/example/hp/isun/SettingActivity.java
+++ app/src/main/java/com/example/hp/isun/SettingActivity.java
@@ -14,12 +14,12 @@ public class SettingActivity extends PreferenceActivity implements Preference.On
     public boolean onPreferenceChange(Preference preference, Object value) {
         String stringValue = value.toString();
 
-        if (preference instanceof ListPreference){
-            ListPreference listPreference = (ListPreference)preference;
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
             int prefIndex = listPreference.findIndexOfValue(stringValue);
-            if (prefIndex >= 0){
+            if (prefIndex >= 0) {
                 preference.setSummary(listPreference.getEntries()[prefIndex]);
-            }else {
+            } else {
                 preference.setSummary(stringValue);
             }
         }
@@ -27,17 +27,22 @@ public class SettingActivity extends PreferenceActivity implements Preference.On
         return true;
     }
 
-    private void bindPreferenceSummaryToValue(Preference preference){
+    private void bindPreferenceSummaryToValue(Preference preference) {
         preference.setOnPreferenceChangeListener(this);
 
         onPreferenceChange(preference, PreferenceManager
-        .getDefaultSharedPreferences(preference.getContext())
-        .getString(preference.getKey(), ""));
+                .getDefaultSharedPreferences(preference.getContext())
+                .getString(preference.getKey(), ""));
 
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref_general);
+
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+
     }
 }
